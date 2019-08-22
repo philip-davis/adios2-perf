1be0343764a0210f6342b96f148203ba794121b6
Philip Davis <philipdavis01@gmail.com>
2019-08-22 07:38:57 -0700

    Added gray scott job generator

---
+++ tests/gray-scott/mkjob.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+SCALING=$1
+ENGINE=$2
+NWRITERS=$3
+
+SIZE=
+
+if [ "$SCALING" == "strong" ] ; then
+    SIZE=128
+elif [ "$SCALING" == "weak" ] ; then
+    SIZE=128
+    while [ $((SIZE * SIZE * SIZE)) -lt $((128 * 128 * NWRITERS)) ] ; do
+        SIZE=$((SIZE + 32))
+    done
+    if [ $((SIZE * SIZE * SIZE)) -gt $((128 * 128 * NWRITERS)) ] ; then
+        echo "bad nwriter!"
+        exit
+    fi
+else exit
+fi
+
+WNODES=$((NWRITERS / 32))
+NREADERS=$((NWRITERS / 32))
+RNODES=$((NREADERS / 32))
+while [ $((32 * RNODES)) -lt $NREADERS ] ; do
+    RNODES=$((RNODES + 1))
+done
+
+NNODES=$((WNODES+RNODES))
+DIR_NAME=${SCALING}_${ENGINE}_${NWRITERS}
+
+if [ ! -d $DIR_NAME ] ; then
+    mkdir $DIR_NAME
+fi
+
+export NNODES
+envsubst '${NNODES}' < cfg/cori.header > $DIR_NAME/job.sh
+
+export WNODES RNODES NWRITERS NREADERS
+envsubst '$WNODES $RNODES $NWRITERS $NREADERS' < cfg/job.${ENGINE} >> $DIR_NAME/job.sh
+
+export SIZE ENGINE
+envsubst '$SIZE $ENGINE' < cfg/cfg.json > $DIR_NAME/cfg.json
+cp cfg/${ENGINE}.xml $DIR_NAME/adios2.xml
