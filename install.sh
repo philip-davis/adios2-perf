2c77b01842904db264cb3fbb77756f2b185634ad
Philip Davis <philipdavis01@gmail.com>
2019-08-22 18:59:50 +0000

    Added theta to install script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-MACHINE=cori
+MACHINE=theta
 
 ADIOS2_DIR=${PWD}/install/ADIOS2
 
@@ -7,21 +7,25 @@ if [ "${MACHINE}" == "cori" ] ; then
     module load cmake/3.14.4
     module load python/3.6-anaconda-4.4
     export CRAYPE_LINK_TYPE=dynamic
+elif [ "${MACHINE}" == "theta" ] ; then
+    module unload darshan
+    module load cmake/3.14.5
+    module load cray-python/3.6.5.3
 fi
 
 if [ ! -d ${ADIOS2_DIR} ] ; then
     mkdir ${ADIOS2_DIR}
 fi
 cd ADIOS2
-#rm -rf build
-#mkdir build
+rm -rf build
+mkdir build
 cd build
-#cmake .. -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_BUILD_TYPE=shared -DCMAKE_INSTALL_PREFIX=${ADIOS2_DIR}
-#make
-#make install
+cmake .. -DCMAKE_C_COMPILER=cc -DCMAKE_CXX_COMPILER=CC -DCMAKE_Fortran_COMPILER=ftn -DCMAKE_BUILD_TYPE=shared -DCMAKE_INSTALL_PREFIX=${ADIOS2_DIR}
+make
+make install
 
-#export PYTHONPATH=$PYTHONPATH:${ADIOS2_DIR}/lib64/python3.6/site-packages
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${ADIOS2_DIR}/lib64
+export PYTHONPATH=$PYTHONPATH:${ADIOS2_DIR}/lib64/python3.6/site-packages
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${ADIOS2_DIR}/lib64
 cd ../../adiosvm/Tutorial/gray-scott
 rm -rf build
 mkdir build
